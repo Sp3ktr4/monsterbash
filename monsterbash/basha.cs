67bebf16124db849bfc1c44b8cb264968bdf770e
Spektra <bergman.william@elev.ga.ntig.se>
2022-08-30 13:20:00 +0200

    basha

---
+++ monsterbash/basha.cs
@@ -0,0 +1,8 @@
+using System;
+using System.IO;
+
+public class Basha
+{
+    public int Health = 100;
+    public string name = "Basha";
+}
---
+++ monsterbash/monstros.cs
@@ -0,0 +1,8 @@
+using System;
+using System.IO;
+
+public class Monstros
+{
+    public int health = 100;
+    public string name = "Monstros";
+}
