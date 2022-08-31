2cb7978d54749fd59b2223b8b451910873415ad5
Spektra <bergman.william@elev.ga.ntig.se>
2022-08-31 12:39:41 +0200

    basha2

--- monsterbash/Program.cs
+++ monsterbash/Program.cs
@@ -4,5 +4,15 @@ using System.IO;
 Basha theBasha = new Basha();
 Monstros theMonstros = new Monstros();
 
+Console.WriteLine($"{theMonstros.name} has {theMonstros.health} HP");
+Console.WriteLine("Attack? (Y = Any key pressed)");
 
+Console.ReadLine();
+
+Console.WriteLine("Attacking...");
+theMonstros.health -= 10;
+Console.WriteLine("Montros lost 10 HP");
+Console.WriteLine("Press any key to shutdown...");
+
+Console.ReadLine();
 
